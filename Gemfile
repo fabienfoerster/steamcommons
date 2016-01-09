968ec3261381de24915e8d038f651628ca3af789
Fabien Foerster <fabienfoerster@gmail.com>
2016-01-09 16:29:39 +0100

    usable with console interface

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
 ruby '2.3.0'
+
 gem 'steam-api'
+gem 'highline'
--- steamcommons.rb
+++ steamcommons.rb
@@ -1,23 +1,34 @@
 #!/usr/bin/env ruby
 require "steam-api"
-require 'json'
+require 'highline'
 
-currentId = Steam::User.vanity_to_steamid("fabienfoerster")
+cli = HighLine.new
+
+vanityName = cli.ask "What is your vanity name ?"
+
+currentId = Steam::User.vanity_to_steamid(vanityName)
 params = {"include_appinfo" => 1}
 currentUserGames = Steam::Player.owned_games(currentId, params: params)
 
 games =  currentUserGames['games'].map { |g| g['name'] }
 
 
-
-
 friendsids = Steam::User.friends(currentId, relationship: :friend).map { |f| f['steamid']  }
 
 
 friends = Steam::User.summaries(friendsids)
-friends = friends.map { |f| {f['personaname'] => f['steamid'] } }
-
-friendId = 76561198109436762
+friends = friends.map { |f| [f['personaname'], f['steamid'] ] }.flatten
+hashFriends = Hash[*friends]
+friendId = 0
+cli.choose do |menu|
+  menu.prompt = "Who do you want to know games in common with ?  "
+  hashFriends.keys.each { |f|
+    menu.choice(f) {
+      friendId = hashFriends[f]
+      cli.say("These are the games in commons between you and #{f} \n\n")
+    }
+  }
+end
 
 friendGames = Steam::Player.owned_games(friendId, params: params)
 friendGames = friendGames['games'].map { |g| g['name']  }
